160eafac280ce332f7d7f66ac753a21351d2866f
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-10-26 23:06:21 +0530

    feat: add exception middleware

---
+++ src/middlewares/exception.middleware.ts
@@ -0,0 +1,47 @@
+import { Context } from 'hono';
+import { env } from '../env';
+import { getCookie } from 'hono/cookie';
+import { HTTPException } from 'hono/http-exception';
+import { HTTP_RESPONSE_CODE } from '../constant';
+import { verify } from 'hono/jwt';
+import { getSessionByToken } from '../lib/db/queries/session.queries';
+import { User, Session } from '../lib/db/schema';
+
+declare module 'hono' {
+    interface ContextVariableMap {
+        user?: User;
+        session?: Session;
+    }
+}
+export const auth = async (c: Context, next: any) => {
+    const token = getCookie(c, env.accessTokenName) ?? c.req.header('Authorization')?.split(' ')[1];
+    if (!token || !token.trim()) {
+        throw new HTTPException(HTTP_RESPONSE_CODE.UNAUTHORIZED, {
+            message: 'Unauthorized! Please login',
+        });
+    }
+
+    const payload = await verify(token, env.jwtSecret);
+    if (typeof payload === 'string' || !payload.user) {
+        throw new HTTPException(HTTP_RESPONSE_CODE.UNAUTHORIZED, {
+            message: 'Unauthorized! Please login',
+        });
+    }
+
+    const session = await getSessionByToken(token);
+    if (!session) {
+        throw new HTTPException(HTTP_RESPONSE_CODE.UNAUTHORIZED, {
+            message: 'Session has expired! Please login',
+        });
+    }
+
+    if (new Date() > session.expiresAt || session.revoked) {
+        throw new HTTPException(HTTP_RESPONSE_CODE.UNAUTHORIZED, {
+            message: 'Session has expired! Please login',
+        });
+    }
+
+    c.set('user', payload.user as User);
+    c.set('session', session as Session);
+    await next();
+};
