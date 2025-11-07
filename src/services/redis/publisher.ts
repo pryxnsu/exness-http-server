6dd183e77b9e34f844dab8873d65ffea65dbf2ff
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-11-07 21:58:47 +0530

    feat: add function to publish data to channels

---
+++ src/services/redis/publisher.ts
@@ -0,0 +1,10 @@
+import { publisher } from ".";
+
+export function publish<T>(channel: string, data: T): void {
+    try {
+        publisher.publish(channel, JSON.stringify(data));
+    } catch (err: unknown) {
+        console.error(`[Publisher Error] failed to publish data channel: ${channel}`);
+        throw new Error(`Failed to publish data channel: ${channel}`)
+    }
+}
